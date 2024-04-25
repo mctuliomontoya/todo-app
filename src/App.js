39ee554ee90e82ebe25f6904cccf14894d796b77
mctuliomontoya <mctulio.2@gmail.com>
2024-04-24 19:04:51 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import Modal from "./Modal";
+import "./styles/styles.css";
+import Todo from "./components/Todo";
 
-function App() {
+export default function App() {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const handleOpen = () => setIsOpen(true);
+  const handleClose = () => setIsOpen(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Todo></Todo>
+    </main>
   );
 }
-
-export default App;
